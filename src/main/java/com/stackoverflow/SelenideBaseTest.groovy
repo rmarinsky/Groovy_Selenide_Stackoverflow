6a68a699df44a0804c931a62b6a5ebb629f5e781
rmarinsky <HB_19wRM>
2017-01-16 12:53:19 +0200

    Add web driver manager

--- src/main/java/com/stackoverflow/SelenideBaseTest.groovy
+++ src/main/java/com/stackoverflow/SelenideBaseTest.groovy
@@ -3,6 +3,7 @@ package com.stackoverflow
 import com.codeborne.selenide.Configuration
 import com.stackoverflow.listeners.ScreenshotOnFailure
 import com.stackoverflow.utils.PropertiesReader
+import io.github.bonigarcia.wdm.ChromeDriverManager
 import org.testng.annotations.AfterClass
 import org.testng.annotations.BeforeClass
 import org.testng.annotations.Listeners
@@ -14,7 +15,9 @@ class SelenideBaseTest {
 
     @BeforeClass(alwaysRun = true)
     void setUp() {
-        Configuration.baseUrl = PropertiesReader.getConfig("BROWSER")
+        Configuration.browser = PropertiesReader.getConfig("BROWSER")
+        Configuration.baseUrl = PropertiesReader.getConfig("SITE_URL")
+        ChromeDriverManager.getInstance().setup()
     }
 
     @AfterClass(alwaysRun = true)
